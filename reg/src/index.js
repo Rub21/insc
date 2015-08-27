3ac339bd20a247a2968fc3784e75be062915d6fc
Rub21 <ruben@mapbox.com>
2015-08-26 22:40:19 -0500

    add point

--- reg/src/index.js
+++ reg/src/index.js
@@ -6,7 +6,28 @@ var mapid = 'ruben.n9e0l5jk';
 L.mapbox.accessToken = token;
 var map = L.mapbox.map('map', mapid, {
 	infocontrol: true,
-	zoomControl: true,	
-	center: [51.505, -0.09],
-	zoom: 13
+	zoomControl: true,
+	center: [-13, -74],
+	zoom: 10
+});
+var assetLayerGroup = L.layerGroup().addTo(map);
+map.on('click', function(e) {
+	//map.panTo(e.latlng);
+	assetLayerGroup.clearLayers();
+
+	var latlon = e.latlng.toString().replace('LatLng(', '').replace(')', '').replace(' ', '').split(',').reverse();
+	var point = {
+		"type": "FeatureCollection",
+		"features": [{
+			"type": "Feature",
+			"properties": {},
+			"geometry": {
+				"type": "Point",
+				"coordinates": latlon
+			}
+		}]
+	}
+	L.geoJson(point, {
+		style: L.mapbox.simplestyle.style
+	}).addTo(assetLayerGroup);
 });
